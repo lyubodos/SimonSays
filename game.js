6eb7ce6f2c3d70d804c9678a1b4b874189113716
Lyubomir <lyubomir_vasilev1992@abv.bg>
2020-11-30 19:09:58 +0200

    Delete unnecessary comment

--- game.js
+++ game.js
@@ -52,7 +52,7 @@ function checkAnswer(currentLevel) {
 
       $("#level-title").text("Game Over, Press Any Key to Restart");
 
-      //2. Call startOver() if the user gets the sequence wrong.
+ 
       startOver();
     }
 
@@ -84,10 +84,9 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-//1. Create a new function called startOver().
+//1. Creating a new function called startOver().
 function startOver() {
 
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
   level = 0;
   gamePattern = [];
   started = false;
